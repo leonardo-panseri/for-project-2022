07e463a1adeb4fc7b79bd21737309e4a5c7f034d
viols-code <viola.renne@gmail.com>
2022-06-07 18:21:57 +0200

    Add comments

--- model/vehicle_routing_model.py
+++ model/vehicle_routing_model.py
@@ -5,7 +5,10 @@ from itertools import chain, combinations
 
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    """
+       Find all the subsets of the given set
+       :param iterable: the starting set
+       """
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
